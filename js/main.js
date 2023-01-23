80cf0d19e4a4552504389056ac467d1a7ef53fb0
Zaur <zaur.quseynzade@gmail.com>
2023-01-23 17:38:11 +0400

    dark theme script added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,54 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
 
+
+//use browser theme
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+}
+
+
+
+//Use local storage
+if(localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+}
+else if (localStorage.getItem('darkMode') === 'light'){
+    btnDarkMode.classList.remove('dark-mode-btn--active');
+    document.body.classList.remove('dark');
+}
+
+
+
+//use browser theme change
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e)=>{
+    const newColorScheme = e.matches ? 'dark' : 'light';
+
+    if(newColorScheme === 'dark'){
+        btnDarkMode.classList.add('dark-mode-btn--active');
+        document.body.classList.add('dark');
+        localStorage.setItem('darkMode', 'dark');
+    }else{
+        btnDarkMode.classList.remove('dark-mode-btn--active');
+        document.body.classList.remove('dark');
+        localStorage.setItem('darkMode', 'light');
+    }
+})
+
+
+//Turn on dark mode
 btnDarkMode.addEventListener('click',()=>{
     btnDarkMode.classList.toggle('dark-mode-btn--active');
-    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.toggle('dark');
+
+
+    if (isDark){
+        localStorage.setItem('darkMode', 'dark');
+    } else{
+        localStorage.setItem('darkMode', 'light');
+    }
+
+
 })
